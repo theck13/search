663e015e7b1112e38a821595d01cbde609e6fd4a
Tyler Heck <theck13@users.noreply.github.com>
2025-09-07 10:53:47 -0600

    Add elevation object

---
+++ lib/src/main/kotlin/com/heckofanapp/search/Elevation.kt
@@ -0,0 +1,17 @@
+package com.heckofanapp.search
+
+import androidx.compose.ui.unit.dp
+
+/**
+ * Public version of [Material 3 elevation tokens](https://m3.material.io/styles/elevation/tokens).
+ * Remove if [issue to make elevation tokens public](https://issuetracker.google.com/issues/218092223) is implemented.
+ * See [ElevationTokens.kt source code](https://cs.android.com/androidx/platform/frameworks/support/+/d95b985e08c10ac8d336464be703858819ecd87f:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ElevationTokens.kt) for details.
+ */
+object Elevation {
+    val Level0 = 0.dp // https://cs.android.com/androidx/platform/frameworks/support/+/d95b985e08c10ac8d336464be703858819ecd87f:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ElevationTokens.kt;l=24
+    val Level1 = 1.dp // https://cs.android.com/androidx/platform/frameworks/support/+/d95b985e08c10ac8d336464be703858819ecd87f:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ElevationTokens.kt;l=25
+    val Level2 = 3.dp // https://cs.android.com/androidx/platform/frameworks/support/+/d95b985e08c10ac8d336464be703858819ecd87f:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ElevationTokens.kt;l=26
+    val Level3 = 6.dp // https://cs.android.com/androidx/platform/frameworks/support/+/d95b985e08c10ac8d336464be703858819ecd87f:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ElevationTokens.kt;l=27
+    val Level4 = 8.dp // https://cs.android.com/androidx/platform/frameworks/support/+/d95b985e08c10ac8d336464be703858819ecd87f:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ElevationTokens.kt;l=28
+    val Level5 = 12.dp // https://cs.android.com/androidx/platform/frameworks/support/+/d95b985e08c10ac8d336464be703858819ecd87f:compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ElevationTokens.kt;l=29
+}
