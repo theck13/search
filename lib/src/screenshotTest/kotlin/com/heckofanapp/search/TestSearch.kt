122eb6488f4b91c25c7445bb6bc290cae8ae8a0c
Tyler Heck <theck13@users.noreply.github.com>
2025-09-09 09:43:13 -0600

    Add screenshot tests for search component

---
+++ lib/src/screenshotTest/kotlin/com/heckofanapp/search/TestSearch.kt
@@ -0,0 +1,18 @@
+package com.heckofanapp.search
+
+import androidx.compose.runtime.Composable
+import com.android.tools.screenshot.PreviewTest
+
+@PreviewDarkLight
+@PreviewTest
+@Composable
+fun TestSearchPhone() {
+    PreviewSearchPhone()
+}
+
+@PreviewDarkLight
+@PreviewTest
+@Composable
+fun TestSearchTablet() {
+    PreviewSearchTablet()
+}
